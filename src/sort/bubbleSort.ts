bafc485be892861fce7261707781139d97185ece
Dirk Metzler <dirkmetzler@icloud.com>
2023-03-04 10:06:01 +0100

    Implement bubble sort

--- src/sort/bubbleSort.ts
+++ src/sort/bubbleSort.ts
@@ -1,4 +1,22 @@
 export default function bubbleSort(arr: Array<number>): Array<number> {
-  console.log("start bubbleSort");
+  let swapped = false;
+  let count = 0;
+
+  while (swapped || count === 0) {
+    swapped = false;
+    count++;
+
+    for (let i = 0; i < arr.length - count; i++) {
+      let j = arr[i];
+      let k = arr[i + 1];
+
+      if (typeof k !== "undefined" && j > k) {
+        arr[i] = k;
+        arr[i + 1] = j;
+        swapped = true;
+      }
+    }
+  }
+
   return arr;
 }
