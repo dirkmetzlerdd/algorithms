48c98bcda16a9d2731ab4f3616c214470371117b
Dirk Metzler <dirkmetzler@icloud.com>
2023-03-05 17:02:31 +0100

    Improve recursive type for nestedSum

--- src/recursion/nestedSum.ts
+++ src/recursion/nestedSum.ts
@@ -1,12 +1,11 @@
-type NestedArrayOrNumber<T> = Array<T> | number;
-type nestedSumInput = Array<NestedArrayOrNumber<NestedArrayOrNumber<number>>>;
+type ValueOrArray<T> = T | Array<ValueOrArray<T>>;
 
-export function nestedSum(arr: nestedSumInput) {
+export function nestedSum(arr: Array<ValueOrArray<number>>) {
   let result: number = 0;
 
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])) {
-      result += nestedSum(arr[i] as nestedSumInput);
+      result += nestedSum(arr[i] as Array<ValueOrArray<number>>);
     } else {
       result += arr[i] as number;
     }
