4a8b358607e179778fc73492359bcdd6bdf86330
Dirk Metzler <dirkmetzler@icloud.com>
2023-03-15 07:53:49 +0100

    Add quickSort.ts

---
+++ src/sort/quickSort.ts
@@ -0,0 +1,17 @@
+export function quickSort(arr: Array<number>): Array<number> {
+  if (arr.length < 2) return arr;
+
+  const pivot = arr[arr.length - 1];
+  const left: Array<number> = [];
+  const right: Array<number> = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
--- src/sort/sort.test.ts
+++ src/sort/sort.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 import bubbleSort from "./bubbleSort";
 import insertionSort from "./insertionSort";
 import mergeSort from "./mergeSort";
+import { quickSort } from "./quickSort";
 
 describe("Sort Array", () => {
   test("bubble sort", () => {
@@ -20,5 +21,11 @@ describe("Sort Array", () => {
     const unsortedArrA = [1, 3, 8, 7, 2, 100, 0, 9, 5, 4, 6];
     const sortedArrA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
     expect(mergeSort(unsortedArrA)).toEqual(sortedArrA);
+  });
+
+  test("quick sort", () => {
+    const unsortedArrA = [1, 3, 8, 7, 2, 100, 0, 9, 5, 4, 6];
+    const sortedArrA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
+    expect(quickSort(unsortedArrA)).toEqual(sortedArrA);
   });
 });
